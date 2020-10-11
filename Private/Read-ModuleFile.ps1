6cccc978145705d61e79e68894c1892794b3ba56
Freddie Sackur <github@dustyfox.uk>
2020-10-11 17:53:58 +0100

    Tidy

--- Private/Read-ModuleFile.ps1
+++ Private/Read-ModuleFile.ps1
@@ -11,35 +11,30 @@ function Read-ModuleFile
         [string]$Path
     )
 
-    begin
-    {
-        $OutputProperties = (
-            @{Name = 'Path'; Expression = {
-                if ([IO.Path]::IsPathRooted($Path))
-                {
-                    $Path
-                }
-                else
-                {
-                    Resolve-Path (Join-Path $PWD $Path)
-                }
-            }},
-            'Bytes'
-        )
-    }
-
     process
     {
+        $Output = 1 | Select-Object (
+            'Path',
+            'Bytes'
+        )
+
         if (-not (Test-Path $Path -PathType Leaf))
         {
             Write-Error "Path '$Path' is not a file."
             return
         }
 
-        $Output = 1 | Select-Object $OutputProperties
+        if (-not [IO.Path]::IsPathRooted($Path))
+        {
+            $Path = (Resolve-Path (Join-Path $PWD $Path)).Path
+        }
 
-        $Bytes = [IO.File]::ReadAllBytes($Output.Path)
+
+        $Bytes = [IO.File]::ReadAllBytes($Path)
+
+        $Output.Path  = $Path
         $Output.Bytes = [Convert]::ToBase64String($Bytes)
+
 
         $Output
     }
