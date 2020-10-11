393a25704b9b1eb07faf022f8f555d9a30c92cd4
Freddie Sackur <github@dustyfox.uk>
2020-10-11 19:28:16 +0100

    Read command supports RelativeTo

--- Private/Read-ModuleFile.ps1
+++ Private/Read-ModuleFile.ps1
@@ -3,34 +3,48 @@ function Read-ModuleFile
     <#
         .SYNOPSIS
         Get metadata and content of a file.
+
+        .PARAMETER RelativeTo
+        Provide a path within which to search for files. Has no effect on absolute paths.
     #>
     [CmdletBinding()]
     param
     (
         [Parameter(Mandatory, ValueFromPipeline)]
-        [string]$Path
+        [string]$Path,
+
+        [Parameter()]
+        [string]$RelativeTo = $PWD.Path
     )
+
+    begin
+    {
+        $RelativeTo      = Resolve-Path $RelativeTo -ErrorAction Stop
+        $RelativePattern = [regex]::Escape($RelativeTo) + [regex]::Escape([IO.Path]::DirectorySeparatorChar)
+    }
+
 
     process
     {
+        if (-not [IO.Path]::IsPathRooted($Path))
+        {
+            $FullPath = Join-Path $RelativeTo $Path
+            $FullPath = (Resolve-Path $FullPath -ErrorAction Stop).Path
+            $Path     = $FullPath -replace $RelativePattern
+        }
+        else
+        {
+            $FullPath = (Resolve-Path $Path -ErrorAction Stop).Path
+        }
+
+
+
         $Output = 1 | Select-Object (
             'Path',
             'Bytes'
         )
 
-        if (-not (Test-Path $Path -PathType Leaf))
-        {
-            Write-Error "Path '$Path' is not a file."
-            return
-        }
-
-        if (-not [IO.Path]::IsPathRooted($Path))
-        {
-            $Path = (Resolve-Path (Join-Path $PWD $Path)).Path
-        }
-
-
-        $Bytes = [IO.File]::ReadAllBytes($Path)
+        $Bytes = [IO.File]::ReadAllBytes($FullPath)
 
         $Output.Path  = $Path
         $Output.Bytes = [Convert]::ToBase64String($Bytes)
